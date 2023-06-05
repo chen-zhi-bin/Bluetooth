718b478ef38f032d5d683ba0141a0cd19735c480
chen-zhi-bin <815172923@qq.com>
2023-06-05 21:25:08 +0800

    添加bt客户端的搜索功能

--- app/src/main/java/com/program/bluetooth/bt/BtClientActivity.java
+++ app/src/main/java/com/program/bluetooth/bt/BtClientActivity.java
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.program.bluetooth.R;
+import com.program.bluetooth.util.BtReceiver;
 
-public class BtClientActivity extends AppCompatActivity {
+public class BtClientActivity extends AppCompatActivity implements BtReceiver.Listener,BtDevAdapter.Listener {
     private TextView mTips;
     private EditText mInputMsg;
     private EditText mInputFile;
     private TextView mLogs;
-    private BtDevAdapter mBtDevAdapter;
+    private BtDevAdapter mBtDevAdapter = new BtDevAdapter(this);
+    private BtReceiver mBtReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,5 +36,23 @@ public class BtClientActivity extends AppCompatActivity {
         mInputMsg = findViewById(R.id.input_msg);
         mInputFile = findViewById(R.id.input_file);
         mLogs = findViewById(R.id.tv_log);
+        mBtReceiver = new BtReceiver(this, this);       //注册蓝牙广播
+        BluetoothAdapter.getDefaultAdapter().startDiscovery();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mBtReceiver);
+    }
+
+    @Override
+    public void foundDev(BluetoothDevice dev) {
+        mBtDevAdapter.add(dev);
+    }
+
+    @Override
+    public void onItemClick(BluetoothDevice dev) {
+
     }
 }
